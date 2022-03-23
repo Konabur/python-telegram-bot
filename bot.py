e2c1f9e8a93c521b1ee3540bf6badb83d600d449
Konabur <54441477+Konabur@users.noreply.github.com>
2022-03-23 14:27:43 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,9 +1,14 @@
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, CallbackContext
 import os
+import time
 
 def start(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text(f'Hello {update.effective_user.first_name}\nI am a sample Telegram bot made with python-telegram-bot!')
+    msg = update.message.reply_text(f'Hello {update.effective_user.first_name}\nI am a sample Telegram bot made with python-telegram-bot!')
+    i = 1440
+    while i > 720:
+        time.sleep(120)
+        msg.edit_text(time.strftime('%H:%M')
 
 updater = Updater(os.environ.get("TOKEN"), use_context=True)
 
